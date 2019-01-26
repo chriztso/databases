64d20ab78d9ac9deb2f32767c7326b88798c0a95
chriztso <christophertso2017@u.northwestern.edu>
2019-01-25 21:46:40 -0800

    Created tables and trying to do post requests

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,37 @@
+DROP DATABASE IF EXISTS chat; 
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  name VARCHAR(35), 
+  rooms VARCHAR(35),
+  createdAt VARCHAR(35)
 );
 
+
 /* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id VARCHAR(35), 
+  rooms VARCHAR(35),
+  messages VARCHAR(35)
+);
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id VARCHAR(35), 
+  users VARCHAR(35),
+  messages VARCHAR(35)
+);
 
-
+CREATE TABLE createdAt (
+  /* Describe your table here.*/
+  id VARCHAR(35), 
+  createdAt VARCHAR(35),
+  messages VARCHAR(35)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
